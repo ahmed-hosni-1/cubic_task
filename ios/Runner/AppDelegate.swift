068d86d5c824989e18372673d89d5fdfc1b941b7
Ahmed Hosni <ahmed.hosni.nemr@gmail.com>
2024-11-10 01:19:13 +0200

    Update AppDelegate.swift

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -18,23 +18,4 @@ import GoogleMaps
   }
 }
 
-// import UIKit
-// import Flutter
-// import GoogleMaps
-//
-// @main
-// @objc class AppDelegate: FlutterAppDelegate {
-//   override func application(
-//     _ application: UIApplication,
-//     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
-//
-//   ) -> Bool {
-//         GMSServices.provideAPIKey("AIzaSyDgZjQ7pdtSIafm-H-9vFYokIJBSC1ivek")
-//
-//     if let apiKey = Bundle.main.object(forInfoDictionaryKey: "MAPS_API_KEY") as? String {
-//     }
-//
-//     GeneratedPluginRegistrant.register(with: self)
-//     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
-//   }
-// }
+
