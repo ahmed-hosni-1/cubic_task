b420d7a578f9b0c00d02a31e84fa55cf2d4575df
ahmed <ahmedhosni803@gmail.com>
2024-11-10 04:37:39 +0200

    finished task - add sheet all Branches - add get my location - add go to nearest branch - add toast to show nearest branch

---
+++ lib/core/utils/keys.dart
@@ -0,0 +1,8 @@
+class AppKeys {
+  AppKeys._();
+  static AppKeys? _instance;
+  static AppKeys get instance => _instance ??= AppKeys._();
+
+  String clientId =
+      "160750313684-mrmvifdje50k8uonmc9kb1avko7t8qsh.apps.googleusercontent.com";
+}
